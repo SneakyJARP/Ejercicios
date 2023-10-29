976a87f066d5e47ffaf4ed49b129cb81b8001168
Jaime Rodríguez <145480603+SneakyJARP@users.noreply.github.com>
2023-10-30 00:11:20 +0100

    Docs: add ex 19

---
+++ Java/ejercicio19.js
@@ -0,0 +1,47 @@
+class Person {
+    _firstName="";
+    _lastName="";
+    _age=0;
+
+    constructor (firstName,lastName, age){
+        this._firstName=firstName;
+        this._lastName=lastName;
+        this._age=age;
+    }
+
+    set firstName (newFirstName) {
+        this._firstName = newFirstName
+    }
+
+    set lastName (newLastName) {
+        this._lastName = newLastName
+    }
+
+    set age (newAge){
+        this._age = newAge
+    }
+
+    get firstName () {
+        return this._firstName
+    }
+
+    get lastName () {
+        return this._lastName
+    }
+
+    get age () {
+        return this._age
+    }
+
+    get fullName() {
+        return this._firstName + " " + this._lastName
+    }
+
+}
+    
+    const person = new Person('Mario', 'Rossi', 25);
+    console.log(person.fullName);
+    
+    person.firstName = 'Maria';
+    person.lastName = 'Verdi';
+    console.log(person.fullName);
